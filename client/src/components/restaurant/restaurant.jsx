459093ab25cbae1d75ab16c2a46ef7def2162e57
Malay <malaykumar2003@gmail.com>
2022-10-04 20:07:15 +0530

    init restaurant page

---
+++ client/src/components/restaurant/restaurant.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import PopularCategories from './restaurant/popular-categories'
+function Restaurant() {
+  return (
+    <div><PopularCategories></PopularCategories></div>
+  )
+}
+
+export default Restaurant
---
+++ client/src/components/restaurant/restaurant/popular-categories.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/material";
+function PopularCategories() {
+  return (
+    <Box>
+      <Typography variant="h3" mt={2} fontFamily="Raleway, sans-serif" fontWeight="700">
+        Popular Categories
+      </Typography>
+    </Box>
+  );
+}
+
+export default PopularCategories;
